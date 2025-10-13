e9e316ac2c35941d80982cfd7599cfad714f823c
Genilson Monteiro da Silva <programationwork@gmail.com>
2025-10-13 20:34:32 -0300

    Usando imagem direto da internet

--- cursor-udemy-react-native/App.js
+++ cursor-udemy-react-native/App.js
@@ -1,12 +1,18 @@
 import React, {Component} from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 
 class App extends Component {
   render(){
     return (
-      <View>
-        <Text>Hello, World!</Text>
+      <View style={{backgroundColor: "#a1b3c5ff", flex: 1, justifyContent: "center", alignItems: "center"}}>
+        <Text style={{backgroundColor: "#1e2e3e"}}>Hello, World!</Text>
+        <Text>Hello, Programado!!!</Text>
+        <Text>Hello, Grana Dura de se ganhar!</Text>
+        <Text style={{color: "white", fontSize: 32, margin: 25}}>Hello, Futuro!!!</Text>
+
+        <Image source={{uri: "https://reactnative.dev/docs/assets/p_cat2.png"}} style={{width: 200, height: 200}} />
       </View>
+
     )
   }
 }
