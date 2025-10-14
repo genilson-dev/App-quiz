a59cce228163a2a163256da4d10cb0002c018a94
Genilson Monteiro da Silva <programationwork@gmail.com>
2025-10-13 21:02:31 -0300

    Criando um novo componente detro do componente App

--- cursor-udemy-react-native/App.js
+++ cursor-udemy-react-native/App.js
@@ -1,18 +1,29 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { View, Text, Image } from "react-native";
 
 class App extends Component {
-  render(){
+  render() {
+    let name = "Genilson";
+   
+
     return (
-      <View style={{backgroundColor: "#a1b3c5ff", flex: 1, justifyContent: "center", alignItems: "center"}}>
-        <Text style={{backgroundColor: "#1e2e3e"}}>Hello, World!</Text>
+      <View style={{ backgroundColor: "#a1b3c5ff", flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text style={{ backgroundColor: "#1e2e3e" }}>Hello, World!</Text>
         <Text>Hello, Programado!!!</Text>
         <Text>Hello, Grana Dura de se ganhar!</Text>
-        <Text style={{color: "white", fontSize: 32, margin: 25}}>Hello, Futuro!!!</Text>
-
-        <Image source={{uri: "https://reactnative.dev/docs/assets/p_cat2.png"}} style={{width: 200, height: 200}} />
+        <Text style={{ color: "white", fontSize: 32, margin: 25 }}>Hello, Futuro!!!</Text>
+        <Jobs />
       </View>
 
+    )
+  }
+}
+
+class Jobs extends Component {
+  render() {
+     let foto = "https://reactnative.dev/docs/assets/p_cat2.png"
+    return (
+      <Image source={ {uri: foto}} style={{ width: 200, height: 200 }} />
     )
   }
 }
