6849e699391c387161451936262d122a6ca891ca
Genilson Monteiro da Silva <programationwork@gmail.com>
2025-10-14 21:28:06 -0300

    Usando mais uma dica de como usar o state

--- cursor-udemy-react-native/App.js
+++ cursor-udemy-react-native/App.js
@@ -6,29 +6,29 @@ class App extends Component {
     super();
     this.state = {
       title: "Curso de React Native",
-      nome: "Genilson Silva Monteiro",
-      idade: 40,
-      sexo: "M",
-      profissao: "Programador",
+      nome: "",
+      idade: 0,
+      sexo: "",
+      profissao: "",
 
     }
     this.changer = this.changer.bind(this);
     this.clean = this.clean.bind(this);
   }
-      changer(){
+      changer(nome, idade, sexo, profissao){
         this.setState({
-          nome: "Nasterguile Rocha",
-          idade: 400,
-          sexo: "M",
-          profissao: "Programador Senium",
+          nome: nome,
+          idade: idade,
+          sexo: sexo,
+          profissao: profissao,
         })      
     }
     clean(){
       this.setState({
-        nome: "",
-        idade: 0,
-        sexo: "",
-        profissao: "",
+        nome,
+        idade,
+        sexo,
+        profissao
       })      
   }
 
@@ -36,7 +36,7 @@ class App extends Component {
 
     return (
       <View style={{marginTop: 30, padding: 10, margin: 10}}>
-        <Button title="Send" onPress={this.changer}/>
+        <Button title="Send" onPress={() => this.changer('Joao', 30, 'M', 'Vidraceiro')}/>
         <Text style={{fontSize: 30, textAlign: "center"}}>{this.state.title}</Text>
         <Text style={{margin: 4, color: "white", backgroundColor: "red", borderRadius: 5}}>Nome: {this.state.nome}</Text>
         <Text style={{margin: 4, color: "white", backgroundColor: "red", borderRadius: 5}}>Idade: {this.state.idade}</Text>
