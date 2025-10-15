0478e2508f4896cc45cccb3c5d1d7aeb57e3913a
Genilson Monteiro da Silva <programationwork@gmail.com>
2025-10-14 21:11:16 -0300

    Trabalhando funções com o bind()

--- cursor-udemy-react-native/App.js
+++ cursor-udemy-react-native/App.js
@@ -1,35 +1,53 @@
 import React, { Component } from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Button } from "react-native";
 
 class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      title: "Curso de React Native",
+      nome: "Genilson Silva Monteiro",
+      idade: 40,
+      sexo: "M",
+      profissao: "Programador",
+
+    }
+    this.changer = this.changer.bind(this);
+    this.clean = this.clean.bind(this);
+  }
+      changer(){
+        this.setState({
+          nome: "Nasterguile Rocha",
+          idade: 400,
+          sexo: "M",
+          profissao: "Programador Senium",
+        })      
+    }
+    clean(){
+      this.setState({
+        nome: "",
+        idade: 0,
+        sexo: "",
+        profissao: "",
+      })      
+  }
+
   render() {
-    let name = "Genilson";
-   
 
     return (
-      <View style={{ backgroundColor: "#a1b3c5ff", flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text style={{ backgroundColor: "#1e2e3e" }}>Hello, World!</Text>
-        <Text>Hello, Programado!!!</Text>
-        <Text>Hello, Grana Dura de se ganhar!</Text>
-        <Text style={{ color: "white", fontSize: 32, margin: 25 }}>Hello, Futuro!!!</Text>
-        <Jobs largura={250} altura={250} referencia="Foto vindo do react img"/>
+      <View style={{marginTop: 30, padding: 10, margin: 10}}>
+        <Button title="Send" onPress={this.changer}/>
+        <Text style={{fontSize: 30, textAlign: "center"}}>{this.state.title}</Text>
+        <Text style={{margin: 4, color: "white", backgroundColor: "red", borderRadius: 5}}>Nome: {this.state.nome}</Text>
+        <Text style={{margin: 4, color: "white", backgroundColor: "red", borderRadius: 5}}>Idade: {this.state.idade}</Text>
+        <Text style={{margin: 4, color: "white", backgroundColor: "red", borderRadius: 5}}>Sexo: {this.state.sexo}</Text>
+        <Text style={{margin: 4, color: "white", backgroundColor: "red", borderRadius: 5}}>profissao: {this.state.profissao}</Text>
+        <Button title="Clean" onPress={this.clean}/>
       </View>
 
     )
   }
 }
 
-class Jobs extends Component {
-  render() {
-     let foto = "https://reactnative.dev/docs/assets/p_cat2.png"
-    return (
-      <View>
-        <Image source={ {uri: foto}} style={{ width: this.props.altura, height: this.props.largura }} />
-        <Text style={{ fontSize: 28, marginBottom: 15 }}>Trabalhos</Text>
-        <Text>{this.props.referencia}</Text>
-      </View>
-    )
-  }
-}
 
 export default App;
