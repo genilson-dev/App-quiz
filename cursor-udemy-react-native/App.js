7315f795310109c40d493f0fd3c3e9b910f23ecd
Genilson Monteiro da Silva <programationwork@gmail.com>
2025-10-14 21:38:57 -0300

    Trabalhando com stylesheet separadamente do codigo principal

--- cursor-udemy-react-native/App.js
+++ cursor-udemy-react-native/App.js
@@ -1,48 +1,18 @@
 import React, { Component } from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 class App extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      title: "Curso de React Native",
-      nome: "",
-      idade: 0,
-      sexo: "",
-      profissao: "",
-
-    }
-    this.changer = this.changer.bind(this);
-    this.clean = this.clean.bind(this);
-  }
-      changer(nome, idade, sexo, profissao){
-        this.setState({
-          nome: nome,
-          idade: idade,
-          sexo: sexo,
-          profissao: profissao,
-        })      
-    }
-    clean(){
-      this.setState({
-        nome,
-        idade,
-        sexo,
-        profissao
-      })      
-  }
 
   render() {
 
     return (
-      <View style={{marginTop: 30, padding: 10, margin: 10}}>
-        <Button title="Send" onPress={() => this.changer('Joao', 30, 'M', 'Vidraceiro')}/>
-        <Text style={{fontSize: 30, textAlign: "center"}}>{this.state.title}</Text>
-        <Text style={{margin: 4, color: "white", backgroundColor: "red", borderRadius: 5}}>Nome: {this.state.nome}</Text>
-        <Text style={{margin: 4, color: "white", backgroundColor: "red", borderRadius: 5}}>Idade: {this.state.idade}</Text>
-        <Text style={{margin: 4, color: "white", backgroundColor: "red", borderRadius: 5}}>Sexo: {this.state.sexo}</Text>
-        <Text style={{margin: 4, color: "white", backgroundColor: "red", borderRadius: 5}}>profissao: {this.state.profissao}</Text>
-        <Button title="Clean" onPress={this.clean}/>
+      <View style={styles.container}>
+        <Text style={styles.texte1}>Texte 01</Text>
+        <Text style={styles.texte2}>Texte 02</Text>
+        <Text style={styles.texte3}>Texte 03</Text>
+        <Text style={styles.texte4}>Texte 04</Text>
+        <Text>Texte 05</Text>
+
       </View>
 
     )
@@ -51,3 +21,17 @@ class App extends Component {
 
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'green',
+    margin: 10
+  },
+  texte1:{color:'red', fontSize:20},
+  texte2:{color:'blue', fontSize:25},
+  texte3:{color:'yellow', fontSize:30},
+  texte4:{color:'purple', fontSize:35},
+})
