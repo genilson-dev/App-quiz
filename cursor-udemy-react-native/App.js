ab241f464c49d425bef034b04fd826def522d0bf
Genilson Monteiro da Silva <programationwork@gmail.com>
2025-10-15 22:06:22 -0300

    Tamanho dinamicos e tamanhos fixos

--- cursor-udemy-react-native/App.js
+++ cursor-udemy-react-native/App.js
@@ -7,12 +7,9 @@ class App extends Component {
 
     return (
       <View style={styles.container}>
-        <Text style={styles.texte1}>Texte 01</Text>
-        <Text style={styles.texte2}>Texte 02</Text>
-        <Text style={styles.texte3}>Texte 03</Text>
-        <Text style={styles.texte4}>Texte 04</Text>
-        <Text style={[styles.texte4, styles.texte5]}>Texte 05:</Text>
-
+        <View style={styles.app}></View>
+        <View style={styles.app1}></View>
+        <View style={styles.app2}></View>
       </View>
 
     )
@@ -25,14 +22,24 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'cyan',
-    margin: 1
+    backgroundColor: "#1e2e3e",
+    borderRadius: 5,
+    
+   
   },
-  texte1:{color:'red', fontSize:20},
-  texte2:{color:'blue', fontSize:25},
-  texte3:{color:'yellow', fontSize:30},
-  texte4:{color:'purple', fontSize:35, borderBlockEndColor:'black', borderWidth:6, padding:2},
-  texte5:{color: "whi", borderBlockEndColor:'black', borderWidth:6, padding:2, backgroundColor:'yellow' }
+  app:{
+    height: 65,
+    backgroundColor:"#00ff37ff", 
+    
+  },
+  app1:{
+      flex: 1,
+      backgroundColor:"#fbff00ff", 
+
+  },
+    app2:{
+    height: 65,
+    backgroundColor:"#00ff37ff", 
+  }
+
 })
