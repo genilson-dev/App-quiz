c26b49c422f6352ebd5caf5116ff07c34bc8e482
Genilson Monteiro da Silva <programationwork@gmail.com>
2025-10-16 20:31:10 -0300

    Continuação do uso do state e setState

--- cursor-udemy-react-native/App.js
+++ cursor-udemy-react-native/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 
 class App extends Component {
 
@@ -7,9 +7,8 @@ class App extends Component {
 
     return (
       <View style={styles.container}>
-        <View style={styles.app}></View>
-        <View style={styles.app1}></View>
-        <View style={styles.app2}></View>
+        <TextInput style={styles.input} placeholder="Digite seu nome"/>
+
       </View>
 
     )
@@ -22,30 +21,16 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#1e2e3e",
-    borderRadius: 5,
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "center",
-    
+    justifyContent: "center",
    
   },
-  app:{
-    width:50,
-    height: 50,
-    backgroundColor:"#00ff37ff", 
-    
-  },
-  app1:{
-      width:50,
-      height: 50,
-      backgroundColor:"#fbff00ff", 
-
-  },
-    app2:{
-    width:50,
-    height: 50,
-    backgroundColor:"#00ff37ff", 
+  input:{
+    // width: 45,
+    height: 45,
+    borderWidth: 1,
+    borderColor: "#222",
+    margin: 10,
+    fontSize: 20,
+    padding: 10
   }
-
 })
